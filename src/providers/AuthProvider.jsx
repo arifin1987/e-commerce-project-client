26c0367590d796c44b1c6ad8e2f8282d25ce9aa1
Shohidul Arifin <shohidularifin@gmail.com>
2023-08-23 06:31:26 -0700

    context api used in AuthProvider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,10 +1,52 @@
+import { createContext, useEffect } from "react";
+import {  GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut  } from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { useState } from "react";
 
+export const AuthContext = createContext(null);
+const auth= getAuth(app);
+const googleAuthProvider = new GoogleAuthProvider();
 
-const AuthProvider = () => {
+// eslint-disable-next-line react/prop-types
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+
+    const createUser =(email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+    const signIn = (email,password)=>{
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const singnInWithGoogle =()=>{
+        return signInWithPopup(auth,googleAuthProvider)
+
+    }
+    const logOut =()=>{
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+        })
+        return()=>{
+            unsubscribe();
+        }
+    },[])
+
+    const authInfo = {
+        user,
+        createUser,
+        signIn,
+        singnInWithGoogle,
+        logOut
+
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value= {authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
